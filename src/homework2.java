db3df6da52e1c0570aa6db128e77355b08af0b86
Vadim Turanosoff <turanosoff@gmail.com>
2022-06-21 11:59:15 +0300

    initial commit

---
+++ src/homework2.java
@@ -0,0 +1,11 @@
+public class homework2 {
+    public static void main(String[] args) {
+
+        int roublePerMile = 20;// рубли на милю
+        int ticketCost = 2000; // стоимость билета
+        int countMile = ticketCost / roublePerMile;
+        System.out.println("Вам начислено " + countMile + " миль"
+        );
+
+    }
+}
